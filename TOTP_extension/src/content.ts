95d577e76a7843448cf1ca954b54f2f15ada2a34
pbraune <pbraune@student.ethz.ch>
2024-07-14 19:07:21 +0200

    ADD Access site, ref to idpauthapp, all possible buttons select correct one in DOM

--- TOTP_extension/src/content.ts
+++ TOTP_extension/src/content.ts
@@ -1,17 +1,34 @@
 window.addEventListener("load", () => {
-  const fieldId = "challengeQuestionInput";
-  const field: HTMLInputElement = document.getElementById(
-    fieldId
-  ) as HTMLInputElement;
-  const buttonId = "submitButton";
-  const button = document.getElementById(buttonId);
+  let field: HTMLInputElement | null = null;
+  let button: HTMLElement | null = null;
+  if (location.href.includes("https://access.ethz.ch/idpauthapp")) {
+    const fieldId =
+      "com.siemens.dxa.applications.web.authn.challenging.response2";
+    field = document.getElementsByName(fieldId).item(0) as HTMLInputElement;
+    const buttonId = "Login";
+    const button_list = document.getElementsByName(buttonId);
+    console.log(button_list);
+    for (let i = 0; i < button_list.length; i++) {
+      if (
+        button_list.item(i).getAttribute("type") === "submit" &&
+        button_list.item(i).getAttribute("class") === "icon"
+      ) {
+        button = button_list.item(i);
+      }
+    }
+  } else if (location.href.includes("https://idbdfedin16.ethz.ch")) {
+    const fieldId = "challengeQuestionInput";
+    field = document.getElementById(fieldId) as HTMLInputElement;
+    const buttonId = "submitButton";
+    button = document.getElementById(buttonId);
+  }
   let active: boolean = true;
 
   if (field && button && active) {
     chrome.runtime.sendMessage({ action: "generateTOTP" }, (response) => {
       if (response && response.code) {
-        field.value = response.code;
-        button.click();
+        field!.value = response.code;
+        button!.click();
         active = false;
         setTimeout(() => (active = true), 2000);
       }
